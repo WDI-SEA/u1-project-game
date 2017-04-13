4b84a0b689fb1cea9f4f7858ccdcee26dd9ec697
sdkevorkian <sdkevorkian@gmail.com>
2017-04-13 11:14:15 -0700

    got local storage working, now working on making a food menu

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 // things I would like to do
-// store hunger in localStorage
+// store hunger in localStorage - need to calc
 // various foods to feed animal that can restore hunger
+// make class and store each in obj, the array of foods
+// give "condition" property to animal that has a different picture for each state (happy, sad, dead)
+// as you feed animal its experience goes up and you can unlock better foods
+// also exp up as pet "hover" it
+// display exp
 // warning pops up when hunger is near zero
 // (it would be nice if you could hover over meter and it would be specific about time to run out)
-// as you feed animal its experience goes up and you can unlock better foods
-// give "condition" property to animal that has a different picture for each state (happy, sad, dead)
 // eventually can choose animal
 // choose background
 
@@ -29,14 +32,50 @@ class Pet {
 // initialize pet that is used in game
 var pet = new Pet();
 
+//class for the 6 foods that can be fed
+class Food {
+    constructor(name, hungerIncrease) {
+        this.name = name;
+        this.hungerIncrease = hungerIncrease;
+    }
+}
+
+var apple = new Food("apple", 2);
+var fish = new Food("fish", 5);
+var hotdog = new Food("hotdog", 10);
+var burger = new Food("burger", 15);
+var pizza = new Food("pizza", 20);
+var iceCream = new Food("icecream", 25);
+
+var foods = [apple, fish, hotdog, burger, pizza, iceCream];
+
+// generate food menu
+// icons from http://goodstuffnononsense.com/hand-drawn-icons/cooking-icons/
+
+function foodMenuDisplay() {
+    for (var i = 0; i > foods.length; i++) {
+        var newImage = $("<img>").attr("src", `images/${foods[i].name}.png`);
+        var newLi = $("<li>").html(newImage);
+        $("#food-menu").appendChild(newLi);
+    }
+}
+
 // function to be called in Game loop that continually saves pet status
 // and time stamp for hunger calculations
 function saveToLocalStorage() {
     localStorage.pet = JSON.stringify(pet);
     // for calculating hunger between browser loads
     localStorage.timeStamp = Date.now();
+}
 
-    console.dir(localStorage);
+// put hunger calculation here
+function retrievePetFromStorage() {
+    pet = JSON.parse(localStorage.pet);
+    var hungerDecreaseWhileAway = (Date.now() - localStorage.timeStamp) / timeForHungerDecrease;
+    pet.hunger -= Math.floor(hungerDecreaseWhileAway);
+    if (pet.hunger < 0) {
+        death();
+    }
 }
 
 // this function will be called on a set interval after
@@ -44,13 +83,13 @@ function saveToLocalStorage() {
 // a defined time to decrease hunger is passed to the set interval function
 function gameLoop() {
     // when i get to something that needs a different interval, contact Connor
-    pet.hunger -= 1;
     displayHunger();
+    pet.hunger -= 1;
     saveToLocalStorage();
-    console.log(pet);
-    if (pet.hunger === 0) {
+    if (pet.hunger < 0) {
         death();
     }
+
 }
 
 // resets hunger to max value
@@ -61,10 +100,10 @@ function resetHunger() {
 
 // when click button, hunger goes up by one (i want to replace this with a food menu with different restoration values)
 function feedPet() {
+    displayHunger();
     if (pet.hunger < maxHunger) {
         pet.hunger += 1;
     }
-    displayHunger();
 }
 
 // changes the hunger meter
@@ -95,13 +134,14 @@ function initializeGame(e) {
 // as we wont want to reset everything after a user returns to the page
 function startGame() {
     displayGameStart(pet.name);
+    displayHunger();
     intervalID = setInterval(gameLoop, timeForHungerDecrease);
 }
 
 // once game is started, this  arranges the display
 function displayGameStart(name) {
     $("#top-message").text(`${name}, Your Little Pet`);
-    $("form").fadeOut();
+    $("form").hide();
     $(".hunger").removeClass("hidden");
 }
 
@@ -119,10 +159,19 @@ function resetGame() {
 
 
 $(function() {
+    // check if viewer has seen page before
+    var numViews = 0;
+    if (localStorage.viewCount) {
+        retrievePetFromStorage();
+        numViews = parseInt(localStorage.viewCount);
+        startGame();
+    }
+    numViews += 1;
+    localStorage.viewCount = numViews;
+
+
     // start game after get pet name
     $("form").on("submit", initializeGame);
-
     $("#feed-pet").on("click", feedPet);
-
     $(".reset").on("click", resetGame);
 });
