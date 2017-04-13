1bb723e3a36eb0e55e4d417e1fa6134e369d9ce9
sdkevorkian <sdkevorkian@gmail.com>
2017-04-13 14:04:11 -0700

    got food menu working, added purr. last commit before restructure game loop

--- main.js
+++ main.js
@@ -12,7 +12,7 @@
 // choose background
 
 // max set to 24 so it will be easy to decrease every hour?
-var maxHunger = 24;
+var maxHunger = 100;
 // can change this for demo purposes
 // passed along with hunger decreasefunction on a set interval
 var timeForHungerDecrease = 1000;
@@ -52,13 +52,7 @@ var foods = [apple, fish, hotdog, burger, pizza, iceCream];
 // generate food menu
 // icons from http://goodstuffnononsense.com/hand-drawn-icons/cooking-icons/
 
-function foodMenuDisplay() {
-    for (var i = 0; i > foods.length; i++) {
-        var newImage = $("<img>").attr("src", `images/${foods[i].name}.png`);
-        var newLi = $("<li>").html(newImage);
-        $("#food-menu").appendChild(newLi);
-    }
-}
+
 
 // function to be called in Game loop that continually saves pet status
 // and time stamp for hunger calculations
@@ -78,6 +72,16 @@ function retrievePetFromStorage() {
     }
 }
 
+// generates foods from array - useful if i want to add more foods ever
+function foodMenuGenerate() {
+    for (var i = 0; i < foods.length; i++) {
+        var newImage = $("<img>").attr("src", `images/food/${foods[i].name}.png`);
+        newImage.attr("data-array-pos", i);
+        var newLi = $("<li>").html(newImage);
+        $(".food-menu").append(newLi);
+    }
+}
+
 // this function will be called on a set interval after
 // the submit button has been clicked
 // a defined time to decrease hunger is passed to the set interval function
@@ -92,18 +96,33 @@ function gameLoop() {
 
 }
 
-// resets hunger to max value
+// resets pet hunger to max value,
+// sets max and value of hunger-meter so if I want to change the max hunger I don't have to change it in the HTML.
 function resetHunger() {
     pet.hunger = maxHunger;
+    $("#hunger-meter").attr("max", `${maxHunger}`);
     displayHunger();
 }
 
 // when click button, hunger goes up by one (i want to replace this with a food menu with different restoration values)
+
+function shortPurr() {
+    $("#pet").addClass("purr");
+    setTimeout(function() { $("#pet").removeClass("purr"); }, 500);
+}
+
 function feedPet() {
-    displayHunger();
+    var posOfFoodClicked = $(this).data("array-pos");
+    var clickedFood = foods[posOfFoodClicked];
     if (pet.hunger < maxHunger) {
-        pet.hunger += 1;
+        pet.hunger += clickedFood.hungerIncrease;
+        if (pet.hunger > maxHunger) {
+            pet.hunger = maxHunger;
+        }
     }
+    displayHunger();
+    shortPurr();
+    console.log(pet.hunger);
 }
 
 // changes the hunger meter
@@ -113,11 +132,12 @@ function displayHunger() {
 
 // this is what happens when you kill your pet
 function death() {
-    $("#pet").addClass("death");
+    $(".hunger").addClass("hidden");
+    $(".food-menu").addClass("hidden");
+    $("#pet").addClass("death").removeClass("hoverable");
     $(".game-field").addClass("death-bg");
     $("#top-message").fadeOut();
     setTimeout(function() { $("#top-message").text("You have failed your pet...").fadeIn(); }, 500);
-    $(".hunger").addClass("hidden").fadeOut();
     $(".reset").fadeIn();
     clearInterval(intervalID);
 }
@@ -143,8 +163,9 @@ function displayGameStart(name) {
     $("#top-message").text(`${name}, Your Little Pet`);
     $("form").hide();
     $(".hunger").removeClass("hidden");
+    $(".food-menu").addClass("hidden");
+    $("#pet").addClass("hoverable");
 }
-
 
 // restart after pet dies => new page screen
 function resetGame() {
@@ -156,7 +177,9 @@ function resetGame() {
     $(".game-field").removeClass("death-bg");
 }
 
-
+function foodMenuDisplay() {
+    $(".food-menu").toggleClass("hidden");
+}
 
 $(function() {
     // check if viewer has seen page before
@@ -169,9 +192,10 @@ $(function() {
     numViews += 1;
     localStorage.viewCount = numViews;
 
-
+    foodMenuGenerate();
     // start game after get pet name
     $("form").on("submit", initializeGame);
-    $("#feed-pet").on("click", feedPet);
+    $("#feed-pet").on("click", foodMenuDisplay);
     $(".reset").on("click", resetGame);
+    $(".food-menu img").on("click", feedPet);
 });
