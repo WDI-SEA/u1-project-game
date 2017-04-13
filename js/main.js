ff35fce82e165c1efb8c2b76d29e35113f01ffee
markdgold <mgoldstein@utexas.edu>
2017-04-13 16:08:46 -0700

    added styling

--- js/main.js
+++ js/main.js
@@ -1,31 +1,38 @@
-var computersBoard = [
-    ["_", "_", "_", "_", "_", "_"],
-    ["_", "_", "_", "_", "_", "_"],
-    ["_", "_", "_", "_", "_", "_"],
-    ["_", "_", "_", "_", "_", "_"],
-    ["_", "_", "_", "_", "_", "_"],
-    ["_", "_", "_", "_", "_", "_"]
-];
+var computersBoard;
+var computersMap;
+var playersBoard;
 
-var computersMap = [
-    ["_", "_", "_", "_", "_", "_"],
-    ["_", "_", "_", "_", "_", "_"],
-    ["_", "_", "_", "_", "_", "_"],
-    ["_", "_", "_", "_", "_", "_"],
-    ["_", "_", "_", "_", "_", "_"],
-    ["_", "_", "_", "_", "_", "_"]
-];
+function initBoards() {
+    computersBoard = [
+        ["_", "_", "_", "_", "_", "_"],
+        ["_", "_", "_", "_", "_", "_"],
+        ["_", "_", "_", "_", "_", "_"],
+        ["_", "_", "_", "_", "_", "_"],
+        ["_", "_", "_", "_", "_", "_"],
+        ["_", "_", "_", "_", "_", "_"]
+    ];
 
-var playersBoard = [
-    ["_", "_", "_", "_", "_", "_"],
-    ["P", "_", "_", "_", "_", "_"],
-    ["P", "_", "_", "P", "P", "P"],
-    ["P", "_", "_", "_", "_", "_"],
-    ["_", "_", "_", "_", "_", "_"],
-    ["_", "_", "P", "P", "P", "_"]
-];
+    computersMap = [
+        ["_", "_", "_", "_", "_", "_"],
+        ["_", "_", "_", "_", "_", "_"],
+        ["_", "_", "_", "_", "_", "_"],
+        ["_", "_", "_", "_", "_", "_"],
+        ["_", "_", "_", "_", "_", "_"],
+        ["_", "_", "_", "_", "_", "_"]
+    ];
 
+    playersBoard = [
+        ["_", "_", "_", "_", "_", "_"],
+        ["_", "_", "_", "_", "_", "_"],
+        ["_", "_", "_", "_", "_", "_"],
+        ["_", "_", "_", "_", "_", "_"],
+        ["_", "_", "_", "_", "_", "_"],
+        ["_", "_", "_", "_", "_", "_"]
+    ];
+}
 ////////////Make the computer's ships//////////////
+initBoards();
+
 function makeShip(letter) {
     var ranRow = Math.ceil(4 * Math.random());
     var ranCol = Math.ceil(4 * Math.random());
@@ -68,15 +75,62 @@ makeShip('C');
 
 
 function printComputerMap() {
-    $(".0").text(computersMap[0]);
-    $(".1").text(computersMap[1]);
-    $(".2").text(computersMap[2]);
-    $(".3").text(computersMap[3]);
-    $(".4").text(computersMap[4]);
-    $(".5").text(computersMap[5]);
+    $("#map.0").text(computersMap[0]);
+    $("#map.1").text(computersMap[1]);
+    $("#map.2").text(computersMap[2]);
+    $("#map.3").text(computersMap[3]);
+    $("#map.4").text(computersMap[4]);
+    $("#map.5").text(computersMap[5]);
+}
+
+function printComputerBoard() {
+    $("#comp.0").text(computersBoard[0]);
+    $("#comp.1").text(computersBoard[1]);
+    $("#comp.2").text(computersBoard[2]);
+    $("#comp.3").text(computersBoard[3]);
+    $("#comp.4").text(computersBoard[4]);
+    $("#comp.5").text(computersBoard[5]);
 }
 ///////////////////////////////////////////////////
 
+////////////////Make Player Board//////////////////
+var playerUnits = 0;
+
+function printPlayersBoard() {
+    $("#player.0").text(playersBoard[0]);
+    $("#player.1").text(playersBoard[1]);
+    $("#player.2").text(playersBoard[2]);
+    $("#player.3").text(playersBoard[3]);
+    $("#player.4").text(playersBoard[4]);
+    $("#player.5").text(playersBoard[5]);
+}
+
+function markP() {
+    var row = $(this).parent().data('row');
+    var col = $(this).data('num');
+    if (this.getAttribute("data-clicked") === "clicked") {
+
+        $(this).removeClass('goat');
+        this.setAttribute("data-clicked", "notClicked");
+        playersBoard[row][col] = "_";
+        playerUnits -= 1;
+    } else {
+        $(this).addClass('goat');
+        this.setAttribute('data-clicked', "clicked");
+        playersBoard[row][col] = "P";
+        playerUnits += 1;
+    }
+    //printPlayersBoard();
+}
+///////////////////////////////////////////////////
+
+
+///////////////display game over//////////////////
+function gameOver(text, image) {
+    $('.game-over').css('display', 'block');
+    $('.game-over p').text(text);
+    $('.game-over img').attr('src', image);
+}
 
 /////////////////Check who's winning////////////////
 var playerHits = 0;
@@ -84,121 +138,75 @@ var compHits = 0;
 
 function checkWin() {
     if (playerHits === 9) {
-        alert("you win");
+        console.log("PLAYER WINS");
         $('td').off();
+        $("#restart").toggle();
+        gameOver("YOU WON!", './img/good_job.gif');
     } else if (compHits === 8) {
-        alert("you lose");
+        console.log("COMP WINS");
         $('td').off();
+        $("#restart").toggle();
+        gameOver("You lost to a poorly programmed computer. How's that feel?", './img/lose.gif');
     }
 }
 ////////////////////////////////////////////////////
 
 
-
-/*var oneRight = false;
-var firstGuess = [];
-var secondGuess = [];
-var secondGuessOptions = [
-    [1, 0],
-    [-1, 0],
-    [0, 1],
-    [0, -1]
-];
-*/
+////////////////Near Hits////////////////////////////
 var nearHit = "1";
 var hit = "X";
 var miss = "0";
 
-
-/*function compSecondGuess() {
-    var nextGuessDirection = Math.floor(4 * Math.random());
-    console.log(firstGuess);
-    var nextRow = firstGuess[0] + secondGuessOptions[nextGuessDirection][0];
-    var nextCol = firstGuess[1] + secondGuessOptions[nextGuessDirection][1];
-    console.log("secondGuess", nextRow, nextCol);
-    if (nextRow > 5 || nextRow < 0 || nextCol > 5 || nextCol < 0) {
-        console.log("number greater than 5 or smaller than 0")
-        return compSecondGuess();
-    }
-    var secondGuess = playersBoard[nextRow][nextCol];
-    if (secondGuess === "_") { //if empty
-        console.log("2nd Miss");
-        $(`#comp-board tr:eq(${nextRow}) td:eq(${nextCol})`).css('background-color', 'darkblue');
-        playersBoard[nextRow][nextCol] = "miss";
-    } else if (secondGuess === "P") {
-        console.log("2nd hit")
-        $(`#comp-board tr:eq(${nextRow}) td:eq(${nextCol})`).css('background-color', 'red');
-        playersBoard[nextRow][nextCol] = "hit";
-        //twoRight = true;
-        //secondGuess = [nextRow, nextCol];
-        firstGuess = [nextRow, nextCol];
-        return oneRight = true;
-    } else {
-        console.log("bad second guess");
-        compSecondGuess();
-    }
-    oneRight = false;
-
-
-}
-*/
-/*
-function computerGuess() {
-    var ranRow = Math.floor(6 * Math.random());
-    var ranCol = Math.floor(6 * Math.random());
-    if (oneRight) {
-        compSecondGuess();
-    } else {
-        console.log(`computer 1st guess: row ${ranRow}, col ${ranCol}`);
-        if (playersBoard[ranRow][ranCol] === "_") {
-            $(`#comp-board tr:eq(${ranRow}) td:eq(${ranCol})`).css('background-color', 'darkblue');
-            playersBoard[ranRow][ranCol] = "miss";
-        } else if (playersBoard[ranRow][ranCol] === "P") {
-            $(`#comp-board tr:eq(${ranRow}) td:eq(${ranCol})`).css('background-color', 'red');
-            playersBoard[ranRow][ranCol] = "hit";
-            compShipsFound += 1;
-            oneRight = true;
-            firstGuess = [ranRow, ranCol];
-        } else {
-            console.log("bad first guess");
-            computerGuess();
-        }
-    }
-}*/
 var nearHitOptions = [
     [1, 0],
-    [-1, 0],
     [0, 1],
-    [0, -1]
+    [0, -1],
+    [-1, 0]
 ];
 
 
 function markNearHits(row, col) {
     console.log("near hits around", row, col);
+    var loops = 4;
+    var i = 0;
+    if (row === 0) {
+        loops = 3;
+    }
 
-    for (var i = 0; i < 4; i++) {
-        //TODO fix for lowest row
+    if (row === 5) {
+        i = 1;
+    }
+
+    for (i; i < loops; i++) {
         var cellVal = computersMap[row + nearHitOptions[i][0]][col + nearHitOptions[i][1]];
-        if (cellVal !== undefined && cellVal === "_") {
+        if (cellVal === "_") {
             computersMap[row + nearHitOptions[i][0]][col + nearHitOptions[i][1]] = nearHit;
             //$(`#comp-board tr:eq(${row + nearHitOptions[i][0]}) td:eq(${col + nearHitOptions[i][1]})`).css('background-color', 'green');
         }
     }
-    //printComputerMap();
+    printComputerMap();
 }
+//////////////////////////////////////////////////////////
 
+
+
+
+////////////// Mark computer map hit or miss////////////////////
 function markMiss(row, col) {
-    $(`#comp-board tr:eq(${row}) td:eq(${col})`).css('background-color', 'darkblue');
+    $(`#comp-board tr:eq(${row}) td:eq(${col})`).addClass('nope');
     computersMap[row][col] = miss;
 }
 
 function markHit(row, col) {
-    $(`#comp-board tr:eq(${row}) td:eq(${col})`).css('background-color', 'red');
+    $(`#comp-board tr:eq(${row}) td:eq(${col})`).css('background-color', 'rgba(255, 33, 33, .8)');
     computersMap[row][col] = hit;
     compHits += 1;
 }
+///////////////////////////////////////////////////////////////
 
 
+
+///////////////////////Computer's Guess/////////////////////////
 function computerGuess() {
     for (var i = 0; i < 6; i++) {
         for (var j = 0; j < 6; j++) {
@@ -231,30 +239,82 @@ function computerGuess() {
 }
 
 
-
+///////////////////////////Players Guess/////////////////////////////
 function revealSquare() {
-    //console.log($(this).data('ship'));
-    if ($(this).data('ship') === "A") {
-        $(this).css("background-color", "darkgrey");
-        playerHits += 1;
-    } else if ($(this).data('ship') === 'B') {
-        $(this).css("background-color", "darkgrey");
-        playerHits += 1;
+    if (!$(this).hasClass('comp-goat') && !$(this).hasClass('nope')) {
+        if ($(this).data('ship') === "A") {
+            $(this).addClass('comp-goat');
+            playerHits += 1;
+        } else if ($(this).data('ship') === 'B') {
+            $(this).addClass('comp-goat');
+            playerHits += 1;
 
-    } else if ($(this).data('ship') === 'C') {
-        $(this).css("background-color", "darkgrey");
+        } else if ($(this).data('ship') === 'C') {
+            $(this).addClass('comp-goat');
 
-        playerHits += 1;
-    } else {
-        $(this).css("background-color", "darkblue");
+            playerHits += 1;
+        } else {
+            $(this).addClass('nope');
 
+        }
+        checkWin();
+        setTimeout(computerGuess, 800);
     }
-    checkWin();
-    computerGuess();
-    //setTimeout(computerGuess, 1000);
-    printComputerMap();
+}
+/////////////////////////////////////////////////////
+
+function toggleBetweenGames() {
+
+    $('.left').toggle("slide");
+    $('#start').toggle();
+    $('.right h2').slideToggle();
 }
 
-$(function() {
+///////////////////start game////////////////////
+function startGame() {
+    if (playerUnits != 9) {
+        alert("place 9 units only");
+    } else {
+        $('#comp-board td').off();
+        $('h1').toggleClass('color-change');
+        $('p.instructions').toggle();
+        toggleBetweenGames();
+    }
+}
+/////////////////////////////////////////////
+
+////////////////restart game//////////////////////
+function restartGame() {
+    console.log('restart');
+    initBoards();
+    makeShip('A');
+    makeShip('B');
+    makeShip('C');
+    playerUnits = 0;
+    $('td').removeClass();
+    $('td').attr('data-clicked', 'notclicked');
+    $('#comp-board td').on('click', markP);
+
     $('#player-board td').on('click', revealSquare);
+    $("#comp-board td").css('background-color', '');
+    //printComputerBoard();
+
+    $('#restart').toggle();
+    $('.game-over').css("display", 'none');
+    toggleBetweenGames();
+}
+//////////////////////////////////////////////////////////
+
+function pageLoadStuff() {
+    $('.flex-box').slideToggle();
+    $('.flex-box').css('display', 'flex');
+}
+
+
+$(function() {
+    setTimeout(pageLoadStuff, 1000);
+    $('#player-board td').on('click', revealSquare);
+    $('#comp-board td').on('click', markP);
+    $('#start').on('click', startGame);
+    $('#restart').on('click', restartGame);
 });
