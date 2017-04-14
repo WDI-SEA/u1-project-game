4bc1452eabd8087dd0d8ed859542066e39706510
Peter Amadon <peteramadon@Peters-MacBook-Pro.local>
2017-04-14 16:55:33 -0700

    added sounds to .on click functions

--- js/main.js
+++ js/main.js
@@ -4,21 +4,30 @@ var player1Score = 0;
 var player2Score = 0;
 var isPlayerOneTurn = true;
 var gameStarted = false;
-$('#counter span').html(counter);
-$('#timer span').html(seconds);
 
-$('.first-hide').hide('fast');
-$('.first-hide').hide('fast');
-$('.first-hide').hide('fast');
+function onPageLoad() {
+    $('#counter span').html(counter);
+    $('#timer span').html(seconds);
+
+    $('.first-hide').hide();
+    $('.first-hide').hide();
+    $('.first-hide').hide();
+    $('.first-hide').hide();
+}
 
 $('#startgame').on("click", function() {
     gameStarted = true;
-    $(this).hide('fast');
-    $('#endScreen').hide('fast');
-    var randWidth = Math.ceil(Math.random() * 700);
-    var randHeight = Math.ceil(Math.random() * 700);
-
-    $('#bunny').show('fast');
+    $(this).hide();
+    $('#endScreen').hide();
+    $('#start').show();
+    var randWidth = Math.ceil(Math.random() * 500);
+    var randHeight = Math.ceil(Math.random() * 500);
+    var randWidth1 = Math.ceil(Math.random() * 500);
+    var randHeight1 = Math.ceil(Math.random() * 500);
+    $('#tweety-bird').show();
+    $('tweety-bird').css('top', randHeight1);
+    $('tweety-bird').css('right', randWidth1);
+    $('#bunny').show();
     $('#bunny').css('top', randHeight);
     $('#bunny').css('left', randWidth);
 
@@ -28,53 +37,69 @@ $('#startgame').on("click", function() {
     }, 1000);
 });
 
+$('#tweety-bird').on('click', function() {
+    counter = counter - 1;
+    $(this).hide();
+    $('#bunny').hide();
+    $('#start').show();
+    $('#gun-sound')[0].currentTime = 0;
+    $('#gun-sound')[0].play();
+    $('#bird-sound')[0].currentTime = 0;
+    $('#bird-sound')[0].play();
 
+
+});
+
+$('#start').on("click", function() {
+
+
+    var randWidth = Math.ceil(Math.random() * 750);
+    var randHeight = Math.ceil(Math.random() * 750);
+    var randWidth1 = Math.ceil(Math.random() * 550);
+    var randHeight1 = Math.ceil(Math.random() * 550);
+
+    $('#tweety-bird').show();
+    $('#tweety-bird').css('top', randHeight1);
+    $('#tweety-bird').css('left', randWidth1);
+    $('#bunny').show();
+    $('#bunny').css('top', randHeight);
+    $('#bunny').css('left', randWidth);
+    //$(this).hide();
+});
 
 $('#bunny').on("click", function() {
     counter = counter + 1;
     $('#counter span').html(counter);
-
     if (counter >= 20) {
         $('#endScreen').show();
         $('#endCount').html(counter);
         $('#endSeconds').html(seconds);
-        $('#bunny').hide('fast');
-        $('#start').hide('fast');
-        $("#restart-game").show('fast');
+        $('#bunny').hide();
+        $('#start').hide();
+        $('#tweety-bird').hide();
+        $("#restart-game").show();
         if (isPlayerOneTurn) {
             player1Score = counter;
         } else {
             player2Score = counter;
         }
-        if (player1Score > player2Score) {
-            console.log('PLAYER ONE WINS');
-        } else if (player1Score < player2Score) {
-            console.log("Player TWO wins");
-        }
     } else {
-
         $(this).hide();
-        $('#start').show('fast');
+        $('#tweety-bird').hide();
+        $('#start').show();
+        $('#gun-sound')[0].currentTime = 0;
+        $('#gun-sound')[0].play();
 
     }
 });
-$('#start').on("click", function() {
 
-
-    var randWidth = Math.ceil(Math.random() * 700);
-    var randHeight = Math.ceil(Math.random() * 700);
-
-    $('#bunny').show('fast');
-    $('#bunny').css('top', randHeight);
-    $('#bunny').css('left', randWidth);
-    $(this).hide();
-});
 
 $('#restart-game').on('click', function() {
     isPlayerOneTurn = false;
     counter = 0;
     seconds = 0;
-    $(this).hide('fast');
-    $('#bunny').show('fast');
-    $('#endScreen').hide('fast');
+    $(this).hide();
+    $('#bunny').show();
+    $('#endScreen').hide();
 });
+$(onPageLoad);
