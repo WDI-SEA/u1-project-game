9b17eee627bd1e31dad31843545aeb6332c105c6
Peter Amadon <peteramadon@Peters-MacBook-Pro.local>
2017-04-14 14:41:21 -0700

    added two player score check

--- js/main.js
+++ js/main.js
@@ -1,25 +1,24 @@
 var counter = 0;
 var seconds = 0;
-var player1 = 0;
-var player2 = 0;
+var player1Score = 0;
+var player2Score = 0;
+var isPlayerOneTurn = true;
 var gameStarted = false;
-var currentPlayer = player1;
 $('#counter span').html(counter);
 $('#timer span').html(seconds);
 
-$('#start').hide();
-$('#bunny').hide();
-$('#endScreen').hide();
+$('.first-hide').hide('fast');
+$('.first-hide').hide('fast');
+$('.first-hide').hide('fast');
 
 $('#startgame').on("click", function() {
     gameStarted = true;
-    currentPlayer = player1;
-    $(this).hide();
-
+    $(this).hide('fast');
+    $('#endScreen').hide('fast');
     var randWidth = Math.ceil(Math.random() * 700);
     var randHeight = Math.ceil(Math.random() * 700);
 
-    $('#bunny').show();
+    $('#bunny').show('fast');
     $('#bunny').css('top', randHeight);
     $('#bunny').css('left', randWidth);
 
@@ -29,32 +28,53 @@ $('#startgame').on("click", function() {
     }, 1000);
 });
 
+
+
+$('#bunny').on("click", function() {
+    counter = counter + 1;
+    $('#counter span').html(counter);
+
+    if (counter >= 20) {
+        $('#endScreen').show();
+        $('#endCount').html(counter);
+        $('#endSeconds').html(seconds);
+        $('#bunny').hide('fast');
+        $('#start').hide('fast');
+        $("#restart-game").show('fast');
+        if (isPlayerOneTurn) {
+            player1Score = counter;
+        } else {
+            player2Score = counter;
+        }
+        if (player1Score > player2Score) {
+            console.log('PLAYER ONE WINS');
+        } else if (player1Score < player2Score) {
+            console.log("Player TWO wins");
+        }
+    } else {
+
+        $(this).hide();
+        $('#start').show('fast');
+
+    }
+});
 $('#start').on("click", function() {
 
 
     var randWidth = Math.ceil(Math.random() * 700);
     var randHeight = Math.ceil(Math.random() * 700);
 
-    $('#bunny').show();
+    $('#bunny').show('fast');
     $('#bunny').css('top', randHeight);
     $('#bunny').css('left', randWidth);
     $(this).hide();
 });
 
-$('#bunny').on("click", function() {
-    counter = counter + 1;
-    $('#counter span').html(counter);
-
-    if (counter === 20) {
-        $('#endScreen').show();
-        $('#endCount').html(counter);
-        $('#endSeconds').html(seconds);
-        $('#bunny').hide();
-        $('#start').hide();
-    } else {
-
-        $(this).hide();
-        $('#start').show();
-
-    }
+$('#restart-game').on('click', function() {
+    isPlayerOneTurn = false;
+    counter = 0;
+    seconds = 0;
+    $(this).hide('fast');
+    $('#bunny').show('fast');
+    $('#endScreen').hide('fast');
 });
